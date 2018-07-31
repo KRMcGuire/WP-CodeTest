695f3f0d9b46ad6f364ca8ffec046e653d33a23c
Kieran McGuire <kieran.r.mcguire@gmail.com>
2018-07-31 15:43:59 +0000

    Basic plugin functionality.

--- WP-CodeTest.php
+++ WP-CodeTest.php
@@ -2,16 +2,18 @@
 /*
  Plugin Name: Codetest TeamDivision
  */
-defined('ABSPATH') or die('Killed');
 require __DIR__ . '/vendor/autoload.php';
 
 
-function buildTable($attr) {
-    $teamsData = new TeamsData("dfsdfdsf");
-    $data = $teamsData->getData();
-    return $data;
+function WPCTbuildTable() {
+    $teamsData = new WPCT\Supporting\TeamsData("74db8efa2a6db279393b433d97c2bc843f8e32b0");
+
+    $html = $teamsData->getMarkup();
+
+    return $html;
 }
 
-add_shortcode('teamsdata', 'buildTable');
+
+add_shortcode('teamsdata', 'WPCTbuildTable');
 
 ?>
