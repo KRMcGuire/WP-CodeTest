777fb235d15ba8f3efdac0dc78eeab22e3962ee2
Kieran McGuire <kieran.r.mcguire@gmail.com>
2018-07-31 19:19:44 +0000

    Removed commented out line that was no longer needed

--- WP-CodeTest.php
+++ WP-CodeTest.php
@@ -6,7 +6,6 @@ require __DIR__ . '/vendor/autoload.php';
 
 function WPCTbuildTable() {
     $apiKey = get_option("wpct-api-key");
-    //$teamsData = new WPCT\Supporting\TeamsData("74db8efa2a6db279393b433d97c2bc843f8e32b0");
     $teamsData = new WPCT\Supporting\TeamsData($apiKey);
     $html = $teamsData->getMarkup();
 
